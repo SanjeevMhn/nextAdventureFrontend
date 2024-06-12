14210bca3641da38cbddd3ba5eeec58ae8d78a4d
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2024-06-12 17:03:06 +0545

    [ADDED]: added footer (TODO: fix responsive design)

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { HomeService } from '../home/home.service';
+import { Observable, map } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent{
+
+  services$:Observable<string[]> = this.homeService.getServices().pipe(map(items => items));
+
+  constructor(private homeService: HomeService){}
 
 }
