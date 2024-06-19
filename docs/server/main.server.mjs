57adcfefbd41af17139b6fb74458ba6d6008915e
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2024-06-19 11:07:12 +0545

    [FIXED]: fixed index.html file location

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-Y3IOQYX7.mjs";import"./chunk-SWLPIEFP.mjs";import"./chunk-FV6TSPWV.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-5SSP6LT3.mjs";import"./chunk-SWLPIEFP.mjs";import"./chunk-FV6TSPWV.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
