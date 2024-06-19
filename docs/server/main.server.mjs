0274f33fc5122fa42aefe6a427e1aea8ecbb799c
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2024-06-19 11:48:41 +0545

    [FIXED]

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-5W4YOBJP.mjs";import"./chunk-SWLPIEFP.mjs";import"./chunk-FV6TSPWV.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-GMUN5WRC.mjs";import"./chunk-SWLPIEFP.mjs";import"./chunk-FV6TSPWV.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -32,27 +32,27 @@ export class HomeService {
       {
         id: 1,
         name: 'Pokhara',
-        img: 'images/pokhara.jpg'
+        img: './images/pokhara.jpg'
       },
       {
         id: 2,
         name: 'Annapurna',
-        img: 'images/annapurna.jpg'
+        img: './images/annapurna.jpg'
       },
       {
         id: 3,
         name: 'Ghandruk',
-        img: 'images/ghandruk.jpg'
+        img: './images/ghandruk.jpg'
       },
       {
         id: 4,
         name: 'Lumbini',
-        img: 'images/lumbini.jpg'
+        img: './images/lumbini.jpg'
       },
       {
         id: 5,
         name: 'Mardi',
-        img: 'images/mardi.jpg'
+        img: './images/mardi.jpg'
       }
     ];
 
@@ -64,27 +64,27 @@ export class HomeService {
       {
         id: 1,
         name: 'Pokhara',
-        img: 'images/pokhara.jpg'
+        img: './images/pokhara.jpg'
       },
       {
         id: 2,
         name: 'Annapurna',
-        img: 'images/annapurna.jpg'
+        img: './images/annapurna.jpg'
       },
       {
         id: 3,
         name: 'Ghandruk',
-        img: 'images/ghandruk.jpg'
+        img: './images/ghandruk.jpg'
       },
       {
         id: 4,
         name: 'Lumbini',
-        img: 'images/lumbini.jpg'
+        img: './images/lumbini.jpg'
       },
       {
         id: 5,
         name: 'Mardi',
-        img: 'images/mardi.jpg'
+        img: './images/mardi.jpg'
       }
     ];
 
@@ -110,27 +110,27 @@ export class HomeService {
       {
         id: 1,
         name: 'Top hiking locations',
-        img: 'images/pokhara.jpg'
+        img: './images/pokhara.jpg'
       },
       {
         id: 2,
         name: 'Bungee destinations',
-        img: 'images/mardi.jpg'
+        img: './images/mardi.jpg'
       },
       {
         id: 3,
         name: 'Top events in Kathmandu',
-        img: 'images/ghandruk.jpg'
+        img: './images/ghandruk.jpg'
       },
       {
         id: 4,
         name: 'Cable car in Pokhara',
-        img: 'images/lumbini.jpg'
+        img: './images/lumbini.jpg'
       },
       {
         id: 5,
         name: 'Chandragiri temple visit',
-        img: 'images/annapurna.jpg'
+        img: './images/annapurna.jpg'
       }
     ];
 
