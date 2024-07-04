000ed8b3ec8a30de8194e9e87d1cb56d45ac34bf
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2024-07-04 16:51:08 +0545

    [ADDED]: added bottom nav

--- src/app/bottom-nav/bottom-nav.component.ts
+++ src/app/bottom-nav/bottom-nav.component.ts
@@ -8,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrl: './bottom-nav.component.scss'
 })
 export class BottomNavComponent {
+  bottomMenus: Array<{ name: string, link: string }> = [
+    {
+      name: 'home',
+      link: ''
+    },
+    {
+      name: 'search',
+      link: 'search'
+    },
+    {
+      name: 'news',
+      link: 'news'
+    },
+    {
+      name: 'events',
+      link: 'events'
+    },
+    {
+      name: 'profile',
+      link: 'profile'
+    }
+  ]
 
+  activeMenu: string = "home";
+
+
+  changeActiveMenu(name: string):void{
+    this.activeMenu = name;
+  }
 }
