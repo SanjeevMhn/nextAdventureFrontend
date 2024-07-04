d360c786cbabd1077d3add480d88adfd0799b26f
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2024-07-04 18:12:43 +0545

    [FIXED]: fixed bottom nav routing

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-5LEZTUJE.mjs";import"./chunk-2UWFDSWG.mjs";import"./chunk-BYENNDXX.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-6TKBN3SG.mjs";import"./chunk-SWLPIEFP.mjs";import"./chunk-FV6TSPWV.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
