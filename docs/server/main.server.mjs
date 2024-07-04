0e3a07274400be1f5dda28a81c7cf6de12022630
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2024-07-04 17:13:23 +0545

    [ADDED]: added new build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-6B2JQMOO.mjs";import"./chunk-SWLPIEFP.mjs";import"./chunk-FV6TSPWV.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-Y7L6DDSA.mjs";import"./chunk-SWLPIEFP.mjs";import"./chunk-FV6TSPWV.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
