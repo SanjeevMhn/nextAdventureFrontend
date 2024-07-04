48c7a2fc3c7b80c7accc451bd23592a7cfb05c31
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2024-07-04 18:20:19 +0545

    [FIX]: fix

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-6TKBN3SG.mjs";import"./chunk-SWLPIEFP.mjs";import"./chunk-FV6TSPWV.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-NXKBFZ7C.mjs";import"./chunk-SWLPIEFP.mjs";import"./chunk-FV6TSPWV.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
