f2a765b775b5c47f719cfbd6002300ff1815b186
Sanjeev Maharjan <sanjeevmaharjan19@gmail.com>
2024-07-07 00:01:33 +0545

    [ADDED]: added button for bottom nav

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-RCH3A3C5.mjs";import"./chunk-2UWFDSWG.mjs";import"./chunk-BYENNDXX.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-LEIN2SSI.mjs";import"./chunk-SWLPIEFP.mjs";import"./chunk-FV6TSPWV.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
